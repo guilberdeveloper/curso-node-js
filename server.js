ba0c91d64b7e4904af107f312fea3cceaadd684c
guilberdeveloper <guilberprogramadordeveloper@hotmail.com>
2023-08-08 14:58:11 -0300

    Iniciando o server

---
+++ server.js
@@ -0,0 +1,18 @@
+// teste de servidor web com nodejs
+
+const http = require("http")
+const host = "127.0.0.1"
+const port = 3000
+
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('Olá Mundo /n meu primeiro Script')
+})
+
+
+server.listen(port, host, ()=>{
+    console.log("Server NodeJs Rodando...")
+})
